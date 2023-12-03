dd7083b93c068b9cdef8972f4cc153d2ea901752
hyeonprojects <axzswq@gmail.com>
2023-12-03 16:33:23 +0900

    [11-회원-기능-구현] feat: swagger 추가 및 CORS 추가 및 CSRF 보호 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.use(csurf())
+
+    // swagger
+    const config = new DocumentBuilder()
+        .setTitle('Give Heart API')
+        .setDescription('The Give Heart API description')
+        .setVersion('0.1.1')
+        .addTag('give-heart')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    await app.listen(3000);
 }
+
 bootstrap();
