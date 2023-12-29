859fa848acadb488d663f4cdb50b361601790d0e
hyeonprojects <axzswq@gmail.com>
2023-12-29 22:23:27 +0900

    fix: prittier

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,12 @@
-import { Global, Module } from '@nestjs/common';
-import { SharedService } from './service/shared.service';
-import { CacheService } from './cache/cache.service';
-import { redisProvider } from './cache/redis.provider';
+import { Global, Module } from '@nestjs/common'
+import { SharedService } from './service/shared.service'
+import { CacheService } from './cache/cache.service'
+import { redisProvider } from './cache/redis.provider'
 
 @Global()
 @Module({
     providers: [SharedService, CacheService, redisProvider],
     exports: [SharedService, CacheService],
 })
-export class SharedModule {}
+export class SharedModule {
+}
--- src/users/service/users.service.ts
+++ src/users/service/users.service.ts
@@ -7,14 +7,34 @@ import { CreateUserPayload } from '../interface/user.interface';
 export class UsersService {
     constructor(private readonly usersRepository: UsersRepository) {}
 
+    /**
+     * Find user by id
+     * @param id
+     */
     async findUserById(id: string): Promise<User | null> {
         return this.usersRepository.findUserById(id);
     }
 
+    /**
+     * Find user by email
+     * @param email
+     */
     async findUserByEmail(email: string): Promise<User | null> {
         return this.usersRepository.findUserByEmail(email);
     }
 
+    /**
+     * Find user by phone
+     * @param phone
+     */
+    async findUserByPhone(phone: string): Promise<User | null> {
+        return this.usersRepository.findUserByPhone(phone);
+    }
+
+    /**
+     * Find user by email
+     * @param email
+     */
     async createUser(payload: CreateUserPayload): Promise<User | null> {
         return this.usersRepository.createUser({ ...payload });
     }
